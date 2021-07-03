0b11c71aec97a14e88d89f7a49f4d4e10eae4bf5
Rollermix <83765531+Rollermix@users.noreply.github.com>
2021-07-03 14:14:13 +0200

    Add files via upload

--- index.php
+++ index.php
@@ -19,6 +19,28 @@ if (isset($_SESSION["useruid"]))
 }
 
 ?>
-
+<a>Dostępne miasta:</a>
+<table>
+    <tr>
+        <th>
+            Województwo
+        </th>
+        <th>
+            Miasto
+        </th>
+        <th>
+            Opis
+        </th>
+    </tr>
+    <?php
+    $sqli = "SELECT voivodship.name AS 'województwo', city.name, city.description,city.deleted From voivodship Inner Join city ON city.id_voivodship=voivodship.id";
+    $result = mysqli_query($conn, $sqli);
+    while ($row = mysqli_fetch_array($result)) {
+        if($row['deleted']==0) {
+           echo '<tr>'.'<td>'.$row['województwo'].'</td>'.'<td>'.$row['name'].'</td>'.'<td>'.$row['description'].'</td>'.'</tr>';
+        }
+    }
+    ?>
+</table>
 </body>
 </html>
