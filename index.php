36b12fc14872c42a0091dd54438998d97f930edc
Rollermix <83765531+Rollermix@users.noreply.github.com>
2021-07-07 18:40:48 +0200

    Add files via upload

--- index.php
+++ index.php
@@ -19,15 +19,15 @@ if (isset($_SESSION["useruid"]))
 }
 
 ?>
+</br>
+<div>
 <a>Dostępne miasta:</a>
 <table>
     <tr>
         <th>
             Województwo
         </th>
-        <th>
-            Miasto
-        </th>
+        <th>Miasto</th>
         <th>
             Opis
         </th>
@@ -37,10 +37,30 @@ if (isset($_SESSION["useruid"]))
     $result = mysqli_query($conn, $sqli);
     while ($row = mysqli_fetch_array($result)) {
         if($row['deleted']==0) {
-           echo '<tr>'.'<td>'.$row['województwo'].'</td>'.'<td>'.$row['name'].'</td>'.'<td>'.$row['description'].'</td>'.'</tr>';
+           echo '<tr>'.'<td>'.$row['województwo'].'</td>'.'<td>'.'<a href =index.php?city='.$row['name'].'>'.$row['name'].'</a>'.'</td>'.'<td>'.$row['description'].'</td>'.'</tr>';
         }
     }
+
     ?>
 </table>
+</div>
+
+<?php
+    if(isset($_GET['city'])) {
+        $city = ($_GET['city']);
+        $sqli = "SELECT spot.name,spot.description From spot Inner Join city ON spot.id_city=city.id WHERE city.name='" . $city."'";
+        $result = mysqli_query($conn, $sqli);
+        if(mysqli_num_rows($result)>0)
+        {
+            echo '<table>'.'<tr>'.'<th>Nazwa</th>'.'<th>Opis</th>'.'</tr>';
+        while ($row = mysqli_fetch_array($result)) {
+            echo '<tr><td>'.$row['name'].'</td>' .'<td>'. $row['description'] .'</td>' .'</tr>';
+            echo '</table>';
+        }
+    }
+        else
+            echo "Brak miejsc w danym mieście";
+    }
+?>
 </body>
 </html>
