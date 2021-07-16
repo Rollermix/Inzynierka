b4301b20e9ac77e24607d3e44c8b61f7420c3349
Rollermix <83765531+Rollermix@users.noreply.github.com>
2021-07-16 20:21:26 +0200

    Add files via upload

--- findwalk.php
+++ findwalk.php
@@ -10,9 +10,14 @@ require_once 'header.php';
 require_once 'includes/dbh.inc.php';
 ?>
 <?php
+$sql2 = 'SELECT id_city From USER WHERE id="'.$_SESSION['userid'].'"';
+$result2 = mysqli_query($conn, $sql2);
+$row2 = mysqli_fetch_array($result2);
+$mycity = $row2['id_city'];
+
 echo "Znaleziono następujące spacery:<br>";
-$sqli = 'SELECT walk.*,spot.name,user.login From walk INNER JOIN spot ON spot.id=walk.id_spot INNER JOIN user ON user.id=walk.id_user 
-        WHERE walk.id_accompanied_user IS NULL AND id_user!="'.$_SESSION['userid'].'"';
+$sqli = 'SELECT walk.*,spot.name,user.login,user.id_city From walk INNER JOIN spot ON spot.id=walk.id_spot INNER JOIN user ON user.id=walk.id_user 
+        WHERE walk.id_accompanied_user IS NULL AND walk.id_user!="'.$_SESSION['userid'].'" AND user.id_city ="'.$mycity.'"';
 $result = mysqli_query($conn, $sqli);
 while ($row = mysqli_fetch_array($result)) {
     if ($row['id_accompanied_user'] == NULL) {
