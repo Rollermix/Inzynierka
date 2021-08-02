150150051b01f1c104f0ac97627ac6eb2faa9b04
ludwig siarzewsky <pradley1994@gmail.com>
2021-08-02 22:05:00 +0200

    fix: input[type='file'] nie dziala

--- views/containers/footer.php
+++ views/containers/footer.php
@@ -7,5 +7,6 @@
         <script src="<?= baseUrl() ."/assets/js/jquery-3.6.0.js" ?>"></script>
         <script src="<?= baseUrl() ."/assets/js/bootstrap.min.js" ?>"></script>
         <script src="<?= baseUrl() ."/assets/js/bootstrap.bundle.min.js" ?>"></script>
+        <script src="<?= baseUrl() ."/assets/js/custom.js" ?>"></script>
     </body>
 </html>
--- views/contents/editdog.php
+++ views/contents/editdog.php
@@ -1,26 +1,21 @@
 <?php require_once '../containers/header.php'; ?>
 <?php require_once '../containers/menu.php'; ?>
-<?php 
-    
-?>
 <div class="container">
-    <div class="form-group">
-        <form action="<?= baseUrl() . '/includes/editdog.inc.php'?>" method="post" enctype="multipart/form-data">
-            <input class="form-control" type = "text" name="name" placeholder="Jak się wabi twój pies" value="<?= isset($userDogData->name) ? $userDogData->name : "" ?>">
-            <select class="custom-select" name ='size'>
-                <option>Wybierz rozmiar psa...</option>
-                <option>Mały</option>
-                <option>Średni</option>
-                <option>Duży</option>
-            </select>
-            <textarea class="form-control" type = "text" name="opis" placeholder="Opisz Twojego psa"><?= isset($userDogData->opis) ? $userDogData->opis : "" ?></textarea>
-            <div class="custom-file">
-                <input type="file" class="custom-file-input" id="file" name="file">
-                <label class="custom-file-label" for="file">Wybierz zdjęcie</label>
-            </div>
-            <button class="btn btn-dark" type = "submit" name ="submit">Wyślij</button>
-        </form>
-    </div>
+    <section class="dog">
+        <div class="form-group d-flex justify-content-center" id="add-edit-dog">
+            <form action="<?= baseUrl() . '/includes/editdog.inc.php'?>" method="post" enctype="multipart/form-data">
+                <input class="form-control" type = "text" name="name" placeholder="Jak się wabi twój pies" value="<?= isset($userDogData->name) ? $userDogData->name : "" ?>">
+                <select class="custom-select" name ='size'>
+                    <option>Wybierz rozmiar psa...</option>
+                    <option>Mały</option>
+                    <option>Średni</option>
+                    <option>Duży</option>
+                </select>
+                <textarea class="form-control" type = "text" name="opis" placeholder="Opisz Twojego psa"><?= isset($userDogData->opis) ? $userDogData->opis : "" ?></textarea>
+                <input class="form-control" type="file" name="file" >
+                <button class="btn btn-dark" type = "submit" name ="submit">Wyślij</button>
+            </form>
+        </div>
+    </section>
 </div>
-
 <?php require_once '../containers/footer.php'; ?>
