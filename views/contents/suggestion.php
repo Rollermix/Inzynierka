9a9fdb4d9382b1fb17d0edff21109b19cca7e82e
ludwig siarzewsky <pradley1994@gmail.com>
2021-08-21 19:34:49 +0200

    feature: work in proggres - zmiana theme strony

--- views/contents/suggestion.php
+++ views/contents/suggestion.php
@@ -8,21 +8,21 @@ $id = $_SESSION["userid"];
 $sqli = "SELECT blocked From user WHERE id ='".$id."'";
 $result = mysqli_query($conn, $sqli);
 $row = mysqli_fetch_array($result);
-if ($row['blocked']==0) {
-    echo '<h1 class="text-center">Co chciałbyś, aby znalazło się w naszym systemie?</h1>';
-    echo '<p class="text-center">W poniższym formularzu możesz przekazać nam informacje dotyczące rozszerzenia funkcjonalności portalu lub zgłosić błąd, który występuje na stronie.</p>';
-    echo '<div class="form-group d-flex justify-content-center">';
-        echo '<form action="'.baseUrl().'/includes/suggestion.inc.php" method="post">';
-            echo '<textarea class="form-control" type = "text" name="name" rows="15"></textarea>';
-            echo '<button class="btn btn-success" type = "submit" name ="submit">Wyślij</button>';
-        echo '</form>';
-    echo '</div>';
-}
-else if ($row['blocked']==2)
-{
-    echo "<p>Nie możesz wysyłać sugestii.</p>";
-}
-?>
+//if ($row['blocked']==0) {
+//    echo '<h1 class="text-center">Co chciałbyś, aby znalazło się w naszym systemie?</h1>';
+//    echo '<p class="text-center">W poniższym formularzu możesz przekazać nam informacje dotyczące rozszerzenia funkcjonalności portalu lub zgłosić błąd, który występuje na stronie.</p>';
+//    echo '<div class="form-group d-flex justify-content-center">';
+//        echo '<form action="'.baseUrl().'/includes/suggestion.inc.php" method="post">';
+//            echo '<textarea class="form-control" type = "text" name="name" rows="15"></textarea>';
+//            echo '<button class="btn btn-success" type = "submit" name ="submit">Wyślij</button>';
+//        echo '</form>';
+//    echo '</div>';
+//}
+//else if ($row['blocked']==2)
+//{
+//    echo "<p>Nie możesz wysyłać sugestii.</p>";
+//}
+//?>
 <?php
 $id = $_SESSION["userid"];
 $sqli = "SELECT suggestions.id_status,suggestions.suggestion, status.status From suggestions INNER JOIN status 
@@ -75,6 +75,74 @@ if(mysqli_num_rows($result))
     echo '</table>';
 }
 ?>
+<!--    <div class="row">-->
+<!--        <div class="col-md-12">-->
+<!--            <div class="table-wrap">-->
+<!--                <table class="table table-responsive-xl">-->
+<!--                    <thead>-->
+<!--                    <tr>-->
+<!--                        <th>Email</th>-->
+<!--                        <th>Username</th>-->
+<!--                        <th>Status</th>-->
+<!--                        <th>&nbsp;</th>-->
+<!--                    </tr>-->
+<!--                    </thead>-->
+<!--                    <tbody>-->
+<!---->
+<!---->
+<!--                    <tr class="alert" role="alert">-->
+<!--                        <td class="d-flex align-items-center">-->
+<!--                            <div class="img" style="background-image:url(images/xperson_3.jpg.pagespeed.ic.Cln-jaM1jK.webp)"></div>-->
+<!--                            <div class="pl-3 email">-->
+<!--                                <span>larrybird@email.com</span>-->
+<!--                                <span>Added: 01/03/2020</span>-->
+<!--                            </div>-->
+<!--                        </td>-->
+<!--                        <td>Larry_bird</td>-->
+<!--                        <td class="status"><span class="active">Active</span></td>-->
+<!--                        <td>-->
+<!--                            <button type="button" class="close" data-dismiss="alert" aria-label="Close">-->
+<!--                                <span aria-hidden="true"><i class="fa fa-close"></i></span>-->
+<!--                            </button>-->
+<!--                        </td>-->
+<!--                    </tr>-->
+<!--                    <tr class="alert" role="alert">-->
+<!--                        <td class="d-flex align-items-center">-->
+<!--                            <div class="img" style="background-image:url(images/xperson_4.jpg.pagespeed.ic.ucbtJ1Htlo.webp)"></div>-->
+<!--                            <div class="pl-3 email">-->
+<!--                                <span>johndoe@email.com</span>-->
+<!--                                <span>Added: 01/03/2020</span>-->
+<!--                            </div>-->
+<!--                        </td>-->
+<!--                        <td>Johndoe1990</td>-->
+<!--                        <td class="status"><span class="active">Active</span></td>-->
+<!--                        <td>-->
+<!--                            <button type="button" class="close" data-dismiss="alert" aria-label="Close">-->
+<!--                                <span aria-hidden="true"><i class="fa fa-close"></i></span>-->
+<!--                            </button>-->
+<!--                        </td>-->
+<!--                    </tr>-->
+<!--                    <tr class="alert" role="alert">-->
+<!--                        <td class="d-flex align-items-center border-bottom-0">-->
+<!--                            <div class="img" style="background-image:url(images/xperson_1.jpg.pagespeed.ic.a2MnMHMs44.webp)"></div>-->
+<!--                            <div class="pl-3 email">-->
+<!--                                <span>garybird@email.com</span>-->
+<!--                                <span>Added: 01/03/2020</span>-->
+<!--                            </div>-->
+<!--                        </td>-->
+<!--                        <td class="border-bottom-0">Garybird_2020</td>-->
+<!--                        <td class="status border-bottom-0"><span class="waiting">Waiting for Resassignment</span></td>-->
+<!--                        <td class="border-bottom-0">-->
+<!--                            <button type="button" class="close" data-dismiss="alert" aria-label="Close">-->
+<!--                                <span aria-hidden="true"><i class="fa fa-close"></i></span>-->
+<!--                            </button>-->
+<!--                        </td>-->
+<!--                    </tr>-->
+<!--                    </tbody>-->
+<!--                </table>-->
+<!--            </div>-->
+<!--        </div>-->
+<!--    </div>-->
 </div>
 
 <?php require_once '../containers/footer.php'; ?>
